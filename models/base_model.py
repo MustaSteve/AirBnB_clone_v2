41038e6408274dfe21448a49ba07f254b797d6e0
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-22 17:56:45 -0800

    fixing base_model errors

--- models/base_model.py
+++ models/base_model.py
@@ -28,21 +28,27 @@ class BaseModel:
             created_at: creation date
             updated_at: updated date
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            if "updated_at" in kwargs and "updated_at" in kwargs\
+                    and "__class__" in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                del kwargs['__class__']
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """returns a string
