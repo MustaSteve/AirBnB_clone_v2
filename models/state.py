921d0b938a612825de93a58341b5855e72d56c6a
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-22 17:36:08 -0800

    fixing

--- models/state.py
+++ models/state.py
@@ -11,8 +11,9 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
+    __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == "db":
-        __tablename__ = 'states'
+        #__tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
