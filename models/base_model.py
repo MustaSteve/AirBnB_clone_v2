4367f854710093650cd748ca53e2815ee940edc1
Abdelhalim Elbouaami <hbouaami@gmail.com>
2023-11-22 18:07:33 -0800

    adding test

--- models/base_model.py
+++ models/base_model.py
@@ -30,7 +30,7 @@ class BaseModel:
         """
         if not kwargs:
             from models import storage
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
